3447862aa6a821cdca119ff2af57bf1d6ec1c043
Rodrigo Britto Calovi <rodrigo-calovi@uergs.edu.br>
2020-09-16 11:58:48 -0300

    Vetor tamanho fixo

---
+++ Aula1/exerc1.c
@@ -0,0 +1,52 @@
+//1) Dados dois vetores x e y, ambos com n elementos,
+    //determinar o produto escalar desses vetores.
+    //OBS.: o produto escalar é soma de todos os produtos dos
+    //números de mesma posição. Por exemplo, se n = 4:
+    
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+#define TAM 4
+
+int main(void){
+    
+    int vetX[] = {0,3,5,6};
+    int vetY[] = {2,4,9,8};
+    int prodScalar = 0;
+    int i,j;
+
+    printf("\nVetor X: ");
+    for(i=0; i<TAM; i++){
+        if(i<3)
+            printf("%d, ", vetX[i]);
+        if(i==TAM-1)
+            printf("%d ", vetX[i]);
+    }
+
+    printf("\nVetor Y: ");
+    for(i=0; i<TAM; i++){
+        if(i<3)
+            printf("%d, ", vetY[i]);
+        if(i==TAM-1)
+            printf("%d ", vetY[i]);
+    }
+
+    printf("\nProduto escalar X.Y = ");
+    for(i=0; i<TAM; i++){
+        for(j=0; j<TAM; j++){
+            if(i==j && i<TAM-1){
+                printf("(%d x %d) + ",vetX[i], vetY[j]);
+                prodScalar=prodScalar + vetX[i]*vetY[j];
+            } else if(i==j) {
+                printf("(%d x %d) = ",vetX[i], vetY[j]);
+                prodScalar=prodScalar + vetX[i]*vetY[j];
+            }
+        }       
+    }
+    printf("%d\n\n",prodScalar);
+
+    printf("Pressione uma tecla para sair!");
+    getch();
+    return 0;
+}
--- Aula1/hello.c
+++ Aula1/hello.c
@@ -1,8 +1,10 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
 
 int main(void){
+    
 
     printf("Hello World!");
     
