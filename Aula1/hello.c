ed46f568925da6e4d5ac58c9e059f240b8830e1e
Rodrigo Britto Calovi <rodrigo-calovi@uergs.edu.br>
2020-09-17 21:49:48 -0300

    by Rodrigo B. Calovi

--- Aula1/hello.c
+++ Aula1/hello.c
@@ -2,13 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#include <locale.h>
 
 int main(void){
-    
 
+    setlocale(LC_ALL,"portuguese");
+    
+    printf("Olá Mundo!");
+    printf("\n");
     printf("Hello World!");
     
-
+    printf("Pressione uma tecla para continuar!");
     getch();
     
     return 0;
