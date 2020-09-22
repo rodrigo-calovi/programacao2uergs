20cd5920f2a15ad5ee01569d452cf4b28d85bd6b
Rodrigo Britto Calovi <rodrigo-calovi@uergs.edu.br>
2020-09-22 12:58:30 -0300

    By Rodrigo Calovi - Exercicio 8

---
+++ Listas/exerc7.c
@@ -0,0 +1,42 @@
+/**
+** 7.	Leia duas matrizes 4 x 4 e escreva uma terceira com os maiores elementos entre as primeiras.
+** By Rodrigo Calovi
+**/
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+#define TAM 4
+
+
+int main(){
+
+    int MAT1[TAM][TAM] = {
+                            {6,3,74,8},
+                            {10,2,30,4},
+                            {1,20,3,60},
+                            {1,40,7,2}
+                        };
+
+    int MAT2[TAM][TAM] = {
+                            {1,30,6,5},
+                            {6,23,10,54},
+                            {11,24,23,41},
+                            {10,20,71,12}
+                        };
+
+    int MAT3[TAM][TAM];
+
+    int i,j,aux,cont;
+
+
+
+ 
+
+    
+
+
+    printf("\n\nDigite algo para sair!");
+    getch();
+    return 0;
+}
