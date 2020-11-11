e0c452fcd3f1d16e32887b10d38c5cdd24dc49bc
Rodrigo Britto Calovi <rodrigo-calovi@uergs.edu.br>
2020-11-11 11:58:50 -0300

    tamanho vetor

--- Aula9/tamanhoString.c
+++ Aula9/tamanhoString.c
@@ -7,12 +7,30 @@ int tamanhoVetor(char vet[]);
 
 int main (){
     char nome[15];
-    int i;
+    char letra;
+    int i, tamanho, qtRepeticao=0;
+
     printf("Digite um nome: ");
     scanf("%s", &nome);
     fflush(stdin);
+    
     printf("Nome: %s", nome);
-    printf("\nTamanho do nome: %d", tamanhoVetor(nome));
+
+    tamanho=tamanhoVetor(nome);
+    printf("\nTamanho do nome: %d", tamanho );
+
+    printf("\nDigite a letra a substituir: ");
+    letra = getchar();
+    fflush(stdin);
+
+    for(i=0; i<tamanhoVetor(nome); i++){
+        if(nome[i]==letra){
+           nome[i]='*';
+           qtRepeticao += 1;
+        }
+    }
+     printf("Letra '%c', se repete %d vezes.", letra, qtRepeticao);
+     printf("\nNome: %s", nome);
 
     return 0;
 }
