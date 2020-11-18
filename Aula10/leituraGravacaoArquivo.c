b5d212fd76b49c8fc564264a3887be1fbe7e7be1
Rodrigo Britto Calovi <rodrigo-calovi@uergs.edu.br>
2020-11-18 11:08:30 -0300

    Leitura e gravação de arquivo com impressão de tela!

---
+++ Aula10/leituraGravacaoArquivo.c
@@ -0,0 +1,35 @@
+/**
+* Exercício 1: leitura de arquivo texto e gravaçao
+* arquivos necessários:
+* entrada.txt -> onde ficam as entradas de texto
+* saida.txt -> gravação do arquivo final apos leitura de entrada (clone)
+**/
+
+#include <stdio.h>
+
+int main(void){
+
+    FILE *entrada, *saida;
+
+    int letra;
+
+    if ((entrada = fopen("entrada.txt", "r")) == NULL) {
+        printf("ERRO ao abrir arquivo!");
+
+    } else if ((saida = fopen("saida.txt", "w")) == NULL) {
+        printf("ERRO ao abrir arquivo!\n");
+    } else {
+
+        // Lê e grava cada caractere no arquivo
+        while ((letra = fgetc(entrada)) != EOF) {
+            fputc(letra, saida);
+            putchar(letra);
+        }
+
+        fclose(entrada); // Fecha o arquivo entrada
+
+        fclose(saida); // Fecha o arquivo saida
+
+    }
+    return 0;
+}
