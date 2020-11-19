a5c5eb1eac67dfea8a9d367df58002d91b088d81
Rodrigo Britto Calovi <rodrigo-calovi@uergs.edu.br>
2020-11-18 23:04:44 -0300

    Estrutura para WIN ou LINUX em C

---
+++ Aula10/WIN_LINUX.c
@@ -0,0 +1,29 @@
+#ifdef __unix__
+    #include <unistd.h>
+    #include <stdlib.h>
+#elif defined(_WIN32) || defined(WIN32)
+
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <locale.h>
+
+    #define OS_Windows
+
+#endif
+
+#define TAM 5
+
+int main(int argc, char *argv[]) {
+
+    #ifdef OS_Windows
+        /* Codigo Windows */
+        system("cls");
+
+    #else
+        /* Codigo GNU/Linux */
+        system("clear");
+    #endif
+
+
+    return 0;
+}
