b099a0c0af10b1cc4b00be874d5d5e5cba2b8b36
Rodrigo Britto Calovi <rodrigo-calovi@uergs.edu.br>
2020-11-25 10:48:50 -0300

    maiuscula para minuscula

--- Aula11/maiuscula2Minuscula.c
+++ Aula11/maiuscula2Minuscula.c
@@ -8,21 +8,36 @@
 #include <ctype.h>
 
 #define TAM 20
+void minuscula(char palavra[]);
 void maiuscula(char palavra[]);
 
 int main() {
 
     char palavra[TAM];
-    printf("Digite uma palavra:");
+    printf("\nDigite uma palavra:");
     gets(palavra);
-    printf("\nMaiuscula: %s\n", palavra);
-    printf("\nMinuscula: " );
+
+    printf("\nPalavra: \t%s", palavra);
+
+    printf("\nMaiuscula: \t");
     maiuscula(palavra);
+    
+    printf("\nMinuscula: \t" );
+    minuscula(palavra);
+    
     printf("\n");
     return 0;
 }
 
+
 void maiuscula(char palavra[]){
+    int i =0;
+    for(i=0;i<strlen(palavra);i++){
+        printf("%c", toupper(palavra[i]));
+    }
+}
+
+void minuscula(char palavra[]){
     int i =0;
     for(i=0;i<strlen(palavra);i++){
         printf("%c", tolower(palavra[i]));
