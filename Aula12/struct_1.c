cf8f619737299a5a7b2402cd3baa21ae22c4b7e3
Rodrigo Britto Calovi <rodrigo-calovi@uergs.edu.br>
2020-12-02 20:00:16 -0300

    exercicio 1 - aula 12

--- Aula12/struct_1.c
+++ Aula12/struct_1.c
@@ -2,8 +2,8 @@
  * 1) Crie um programa que permita armazenar o nome, a altura e da data de nascimento de até 10 pessoas.
  * Cada pessoa deve ser representada por uma struct dentro de um vetor.
  * O nome, a data de nascimento e a altura de cada pessoa devem ser informados pelo teclado.
- * Posteriormente imprima os nomes das pessoas que possuem altura superior a 1,78.
- */
+ * Posteriormente imprima os nomes das pessoas que possuem altura superior a 1,78. 
+ */ 
 
 #include <stdio.h>
 
@@ -12,6 +12,7 @@ typedef struct dado1{
     float altura;
     char aniver[10];
 } dados;
+
 //typedef struct dados dados;
 
 #define TAM 10
