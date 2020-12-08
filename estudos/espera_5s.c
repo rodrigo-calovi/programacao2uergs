3cf7920a62344e03236538c2b0b5c0c8a4f99577
Rodrigo Britto Calovi <rodrigo-calovi@uergs.edu.br>
2020-12-07 23:51:47 -0300

    Espera de 5s - Windows/Linux

--- estudos/espera_5s.c
+++ estudos/espera_5s.c
@@ -3,17 +3,38 @@
  * Desenvolvido por: Rodrigo Britto Calovi
  */
 
-#include <stdio.h>
-#include <stdlib.h>
+
+#ifdef __unix__
+    #include <unistd.h>
+    #include <stdlib.h>
+    #include <stdio.h>
+
+#elif defined(_WIN32) || defined(WIN32)
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <locale.h>
+
+#define OS_Windows
+
+#endif
+
 
 int main(void) {
+
+#ifdef OS_Windows
+    system("timeout 5");
+    printf("\n");
+
+#else
     printf("\nAguarde o tempo de 5s: ");
     int i = 1;
     while (i <= 5){
         printf("\r\t\t\t%ds",i++);
-        system("sleep 1");
+        system("sleep 1s");
         fflush(stdout);
     }
     printf("\n");
+#endif
+
     return 0;
 }
